727dbb071d13f1df707a9636cb5ccef5bbfe44d0
vikasz1 <mauryvikas12345@gmail.com>
2024-02-06 23:09:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/data")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>React App with MySQL</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
